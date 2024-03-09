1bef8625e29490304e4adb69f3683ad0993bd72a
Nelson Nwajie <nelson@busyqa.com>
2024-03-09 18:02:02 +0000

    added user dike

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@
 resource "aws_iam_user" "users1" {
   name = var.username
 }
+
+resource "aws_iam_user" "dike" {
+  name = var.dike
+}
--- variable.tf
+++ variable.tf
@@ -3,3 +3,9 @@ variable "username" {
   default     = "waleed"
   description = "creating a user"
 }
+
+variable "dike" {
+  type        = string
+  default     = "dike"
+  description = "creating a user"
+}
