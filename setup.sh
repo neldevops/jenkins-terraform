c9fe0047632b959682a468026185ece7760020cc
Nelson Nwajie <nelson@busyqa.com>
2024-03-09 16:59:15 +0000

    added tfe files

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo '...creating files'
+
+touch main.tf
+touch variable.tf
+touch output.tf
+touch terraform.tf
+touch provider.tf
+touch terraform.tfvars
+
+touch Jenkinsfile
+
+
+echo 'all files created successfully'
